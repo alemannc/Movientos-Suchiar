258a0ca50921745cbaa2ed8373162ce4fff5b9db
lerkor-online <lerkor.online@gmail.com>
2024-02-01 09:40:57 -0300

    login and register add

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import './App.css'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 
 const router= createBrowserRouter([
   {
     path:"/",
     element: <Login/> 
+  },
+  {
+    path:"/register",
+    element: <Register/>
   }
 ]); 
 function App() {
 
   return (
    <>
-   <RouterProvider router= {router}/>
+    <RouterProvider router={router}/>
    </>
   )
 }
